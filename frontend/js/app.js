c5cfb2c861736ee126f0548479349770c9d43705
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-07-29 08:26:58 -0700

    the animation are done

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -89,3 +89,33 @@ async function loadWildlife() {
 }
 
 loadWildlife();
+
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach(counter => {
+
+    const updateCounter = () => {
+
+        const target = +counter.dataset.target;
+
+        const count = +counter.innerText;
+
+        const increment = target / 100;
+
+        if(count < target){
+
+            counter.innerText = Math.ceil(count + increment);
+
+            setTimeout(updateCounter,20);
+
+        }else{
+
+            counter.innerText = target;
+
+        }
+
+    };
+
+    updateCounter();
+
+});
