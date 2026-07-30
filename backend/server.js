02a57629ca4068ccdcb8546c4fd058826123999d
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-07-30 09:23:31 -0700

    all the done

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const mysql = require("mysql2");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const path = require("path");
+const bcrypt = require("bcrypt");
 
 dotenv.config();
 
